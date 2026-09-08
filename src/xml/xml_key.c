e2ec936b56e575a88118be41237aea50b758ccd0
SimplyCEO <simplyceo.developer@gmail.com>
2026-09-07 21:21:27 -0300

    Improved `XMLKey` module

--- src/xml/xml_key.c
+++ src/xml/xml_key.c
@@ -5,6 +5,16 @@
 #include "safe_alloc.h"
 #include "toolbox.h"
 
+size_t
+xml_key_size(XMLKey** xml_key)
+{
+  size_t size = 0;
+
+  while (xml_key[size] != NULL) { ++size; }
+
+  return size;
+}
+
 XMLKey**
 xml_key_init(const size_t size)
 {
@@ -23,11 +33,60 @@ xml_key_init(const size_t size)
   return xml_key;
 }
 
+XMLKey**
+xml_key_expand(XMLKey** xml_key, const size_t new_size)
+{
+  size_t i = 0;
+  size_t size = xml_key_size(xml_key);
+
+  /* TODO: Shrink the structure. */
+  if ((size == new_size) || (size > new_size)) { return xml_key; }
+
+  XMLKey** new_xml_key = (XMLKey**)realloc(xml_key, (new_size+1)*sizeof(XMLKey*));
+
+  if (new_xml_key == NULL) { return xml_key; }
+  xml_key = new_xml_key;
+
+  for (i=size; i<new_size; ++i)
+  {
+    xml_key[i] = (XMLKey*)malloc(sizeof(XMLKey));
+    xml_key[i]->name = NULL;
+    xml_key[i]->value = NULL;
+  }
+  xml_key[new_size] = NULL;
+
+  return xml_key;
+}
+
 XMLKey*
 xml_key_set(XMLKey* xml_key, const char* name, const char* value)
 {
+  xml_key->name = safe_free(xml_key->name);
+  xml_key->value = safe_free(xml_key->value);
+
   xml_key->name = strdup(name);
   xml_key->value = strdup(value);
+
+  return xml_key;
+}
+
+XMLKey**
+xml_key_add(XMLKey** xml_key, const char* name, const char* value)
+{
+  if (xml_key == NULL)
+  { return NULL; }
+
+  if (xml_key[0]->value == NULL)
+  {
+    xml_key[0] = xml_key_set(xml_key[0], name, value);
+    return xml_key;
+  }
+
+  size_t size = xml_key_size(xml_key);
+
+  xml_key = xml_key_expand(xml_key, (size+1));
+
+  xml_key[size] = xml_key_set(xml_key[size], name, value);
 
   return xml_key;
 }
--- src/xml/xml_key.h
+++ src/xml/xml_key.h
@@ -14,8 +14,11 @@ typedef struct XMLKey
   char* value;
 } XMLKey;
 
+size_t     xml_key_size(XMLKey** xml_key);
 XMLKey**   xml_key_init(const size_t size);
+XMLKey**   xml_key_expand(XMLKey** xml_key, const size_t new_size);
 XMLKey*    xml_key_set(XMLKey* xml_key, const char* name, const char* value);
+XMLKey**   xml_key_add(XMLKey** xml_key, const char* name, const char* value);
 XMLKey**   xml_key_free(XMLKey** xml_key);
 
 # if defined(__cplusplus)
