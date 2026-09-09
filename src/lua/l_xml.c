ca5d16729b5cc0ee2b67d9dc69bc22f264b52c28
SimplyCEO <simplyceo.developer@gmail.com>
2026-09-08 23:41:19 -0300

    Fixed interpreted XML output not being parsed

--- src/lua/l_xml.c
+++ src/lua/l_xml.c
@@ -28,14 +28,6 @@ l_api_closexml(lua_State* L)
   if (l_xml == NULL)
   { return luaL_error(L, "ERROR: No XML open in memory."); }
 
-  /* View XML file instead of compiling object. */
-  if (compile == false)
-  {
-    lua_pushstring(L, l_xml->xml);
-    xml_free(l_xml);
-    return 1;
-  }
-
   l_xml = xml_parse_fix_structure(l_xml);
   l_xml = xml_parse_identation(l_xml);
 
@@ -44,6 +36,14 @@ l_api_closexml(lua_State* L)
     case -1: l_xml = xml_parse_translate(l_xml, "", ""); break;
     case 1: l_xml = xml_parse_translate(l_xml, "\n", "  "); break;
     default: l_xml = xml_parse_translate(l_xml, "\n", "\t"); break;
+  }
+
+  /* View XML file instead of compiling object. */
+  if (compile == false)
+  {
+    lua_pushstring(L, l_xml->xml);
+    xml_free(l_xml);
+    return 1;
   }
 
   FILE* stream = fopen(l_xml->path, "w");
